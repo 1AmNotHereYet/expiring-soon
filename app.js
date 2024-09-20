61d1476392e4ffca9b85e12077f96492797a02ca
Bilal Ado <1bilalado@gmail.com>
2024-09-20 15:35:00 +0100

    get current date

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 function getCurrentTime() {
   const now = new Date();
+
   return {
     hours: now.getHours(),
     minutes: now.getMinutes(),
@@ -9,3 +10,21 @@ function getCurrentTime() {
 
 const time = getCurrentTime();
 console.log(time);
+
+function getCurrentDate() {
+  const now = new Date();
+
+  const days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+
+  return {
+    dayName: days[now.getDay()],
+    dayNumber: now.getDate(),
+    monthName: months[now.getMonth()],
+    monthNumber: now.getMonth() + 1,
+    year: now.getFullYear()
+  };
+}
+
+const date = getCurrentDate();
+console.log(date);
