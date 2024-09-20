0a4b23e5e9bc8d16d14ab920e7890c8548a7e1b0
Bilal Ado <1bilalado@gmail.com>
2024-09-20 15:40:00 +0100

    get time left till day end

--- app.js
+++ app.js
@@ -28,3 +28,30 @@ function getCurrentDate() {
 
 const date = getCurrentDate();
 console.log(date);
+
+function timeUntilEndOfDay(currentTime) {
+
+  const totalHoursInDay = 24;
+  const totalMinutesInHour = 60;
+  const totalSecondsInMinute = 60;
+
+  const hoursPassed = currentTime.hours;
+  const minutesPassed = currentTime.minutes;
+  const secondsPassed = currentTime.seconds;
+
+  const hoursLeft = totalHoursInDay - hoursPassed - 1;
+  const minutesLeft = totalMinutesInHour - minutesPassed - 1;
+  const secondsLeft = totalSecondsInMinute - secondsPassed;
+
+  const adjustedMinutesLeft = secondsLeft === 60 ? minutesLeft + 1 : minutesLeft;
+  const adjustedHoursLeft = adjustedMinutesLeft === 60 ? hoursLeft + 1 : hoursLeft;
+
+  return {
+    hours: adjustedHoursLeft < 0 ? 0 : adjustedHoursLeft,
+    minutes: adjustedMinutesLeft < 0 ? 0 : adjustedMinutesLeft,
+    seconds: secondsLeft === 60 ? 0 : secondsLeft
+  };
+}
+
+const EndOfDay = timeUntilEndOfDay(time);
+console.log(EndOfDay);
