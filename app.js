cd4ec097d098bc6a006820da2e5dbc0e76e89097
Bilal Ado <1bilalado@gmail.com>
2024-09-21 15:40:00 +0100

    functional tab interface

--- app.js
+++ app.js
@@ -130,3 +130,22 @@ const date = getCurrentDate();
 const EndOfDay = timeUntilEndOfDay(time);
 const EndOfMonth = timeUntilEndOfMonth(date, time);
 const EndOfYear = timeUntilEndOfYear(date, time);
+
+function openTab(event, tabName) {
+
+  var i, tabContent, tabButtons;
+
+  tabContent = document.getElementsByClassName("tab-content");
+  for (i = 0; i < tabContent.length; i++) {
+      tabContent[i].classList.remove("active");
+  }
+
+  tabButtons = document.getElementsByClassName("tab-button");
+  for (i = 0; i < tabButtons.length; i++) {
+      tabButtons[i].classList.remove("active");
+  }
+
+  document.getElementById(tabName).classList.add("active");
+
+  event.currentTarget.classList.add("active");
+}
